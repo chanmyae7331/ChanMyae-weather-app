1d37b40aea3ef926dd7285abad8e0fb3ea996da9
chanmyae7331 <123553053+chanmyae7331@users.noreply.github.com>
2023-06-26 20:54:37 +0630

    Update html,css and javascript

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+function formatDate(date) {
+  let dayIndex = current.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let day = days[dayIndex];
+
+let hours = current.getHours();
+  if(hours <10 ) {
+  hours = `0${hours}`;
+  }
+
+let minutes = current.getMinutes();
+  if (minutes < 10 ) {
+  minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`
+}
+
+let currentDate = document.querySelector("li#date");
+let current = new Date();
+currentDate.innerHTML = formatDate(current);
 //challenge 1
 function currentTemperature(response) {
   let temp = document.querySelector("#temperature");
